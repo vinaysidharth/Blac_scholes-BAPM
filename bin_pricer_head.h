5b62400c8db0acb42ae3be24ab96324e4d520052
Vinay Sidharth <131165424+vinaysidharth@users.noreply.github.com>
2024-12-12 11:54:58 +0000

    Update bin_pricer_head.h

--- bin_pricer_head.h
+++ bin_pricer_head.h
@@ -5,7 +5,7 @@
 #ifndef BIN_PRICER_HEAD_H
 #define BIN_PRICER_HEAD_H
 void get_input(double &R,double &U, double &D, double &n, double &i, double &s0 , double &k); //User input, output type - void
-double crr_pricer(double s0 ,double U, double D , double n,double i );//stock price calculation , output type - double
+double stock_p(double s0 ,double U, double D , double n,double i );//stock price calculation , output type - double
 double risk_neutral_probability(double R,double U,double D);//risk neutral measure estimation , output type - double
 double eur_call_opt(double &k,double &s);//payoff estimation
 #endif //BIN_PRICER_HEAD_H
